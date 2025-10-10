993da11db33f891489ff3bc87895b53a0ecf825f
Shardul Pathak <shardulpathak796@gmail.com>
2025-10-11 01:09:05 +0530

    Update generateSummary.js

--- server/utils/generateSummary.js
+++ server/utils/generateSummary.js
@@ -1,5 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
-const API_KEY = "AIzaSyDCS_LxNpgFo7JVfcVSCRjOEXqlhnWIl8k";
+const API_KEY = "Your_API_Key";
 const ai = new GoogleGenAI({apiKey: API_KEY});
 
 async function generate(title,url) {
