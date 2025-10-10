bb365c3291df71c7303675b103d4bf69ee520f5e
Shardul-Pathak <shardulpathak796@gmail.com>
2025-10-10 12:34:54 +0530

    Back-End Setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+import express from 'express'
+import cors from 'cors'
+import connectDB from './models/connectDB.js'
+import userRoutes from './routes/userRoutes.js'
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+connectDB();
+app.use('/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.end('API is running...');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
